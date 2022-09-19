5622d243f5e1b35a247926afe6cfe144c7cefcc6
Solomasov Denis <denis.solomasov@renault.com>
2022-09-19 18:35:04 +0300

    add design bottom menu

--- app/src/main/java/com/example/budgetio/MainActivity.kt
+++ app/src/main/java/com/example/budgetio/MainActivity.kt
@@ -3,20 +3,31 @@ package com.example.budgetio
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
-        val navController = findNavController(R.id.navigation)
+        val navController = (supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment)
+            .navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.mainMenu)
         bottomNavigationView.setupWithNavController(navController)
+
+        val buttonAddSpending = findViewById<ImageButton>(R.id.addSpending)
+        buttonAddSpending.setOnClickListener {
+
+        }
 
     }
 
